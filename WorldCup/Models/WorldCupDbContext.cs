60733b7edd74223a6da159984ab2fac841e0e935
tonimihajlovski <tonimihajlovski@yahoo.co.uk>
2018-10-01 15:11:31 +0200

    Initial Commit

---
+++ WorldCup/Models/WorldCupDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Reflection.Emit;
+
+namespace WorldCup.Models
+{
+    public class WorldCupDbContext : DbContext
+    {
+        public WorldCupDbContext() : base("WorldCup1")
+        {
+
+        }
+
+        public DbSet<Match> Matches { get; set; }
+        public DbSet<NationalTeam> NationalTeams { get; set; }
+        public DbSet<Player> Players { get; set; }
+
+    }
+}
